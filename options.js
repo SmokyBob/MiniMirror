5bef8177df70c07a04aed07280d5a88fa1690c83
SmokyBob <mauro.solcia@gmail.com>
2015-01-30 01:38:00 +0100

    added colorPicker, change the chromaKey color from the picker

--- options.js
+++ options.js
@@ -1,6 +1,20 @@
 //Use Polymer Bindings
-var mainContent = document.querySelector('#mainContent');
+var mainContent = document.querySelector('#optionContent');
+var mainWindow = null;
 
 mainContent.addEventListener('template-bound', function(){
+  //get the reference to the Main window so that we can set values on it
+  var windowArray = chrome.app.window.getAll();
+  for(var i=0;i<windowArray.length;i++){
+    if(windowArray[i].id!='options'){
+      mainWindow = windowArray[i].contentWindow;
+      break;
+    };
+  }
 
 });
+
+mainContent.colorSelected = function(event){
+  var selColor=event.detail;
+  mainWindow.selectedColor = selColor.rgb;
+};
